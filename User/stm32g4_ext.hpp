c8973c39362fedd9495f793e16b2299b06ccc48f
Jiu-xiao <2592509183@qq.com>
2025-03-30 10:25:24 +0800

    Update LibXR & Fix flash.

--- User/stm32g4_ext.hpp
+++ User/stm32g4_ext.hpp
@@ -7,82 +7,65 @@
 
 class STM32G4Flash : public LibXR::Flash {
  public:
-  STM32G4Flash(size_t start_offset, size_t min_erase_size,
-               size_t min_write_size)
-      : Flash(
-            min_erase_size, min_write_size,
-            LibXR::RawData(reinterpret_cast<void *>(FLASH_BASE + start_offset),
-                           2 * min_erase_size)),
-        start_offset_(start_offset),
-        min_erase_size_(min_erase_size) {}
+  STM32G4Flash(size_t start_offset, size_t min_erase_size, size_t min_write_size)
+      : Flash(min_erase_size, min_write_size,
+              RawData(reinterpret_cast<void *>(FLASH_BASE + start_offset),
+                      2 * min_erase_size)),
+        start_offset_(start_offset) {}
 
   ErrorCode Erase(size_t offset, size_t size) override {
-    if ((offset + size) > 2 * min_erase_size_) {
-      return ErrorCode::OUT_OF_RANGE;
-    }
+    UNUSED(size);
 
-    offset += start_offset_;
+    FLASH_EraseInitTypeDef flash_erase;
+
+    uint32_t sector_error = 0;
 
     HAL_FLASH_Unlock();
 
     __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
 
-    for (size_t addr = offset; addr < offset + size; addr += FLASH_PAGE_SIZE) {
-      FLASH_EraseInitTypeDef erase_init = {};
-      uint32_t sector_error = 0;
+    flash_erase.NbPages = 1;
+    flash_erase.Banks = FLASH_BANK_1;
+    flash_erase.Page = (offset + start_offset_) / FLASH_PAGE_SIZE;
+    flash_erase.TypeErase = FLASH_TYPEERASE_PAGES;
 
-      erase_init.TypeErase = FLASH_TYPEERASE_PAGES;
-      erase_init.Page = (addr - FLASH_BASE) / FLASH_PAGE_SIZE;
-      erase_init.NbPages = 1;
-      erase_init.Banks = FLASH_BANK_1;
-
-      if (HAL_FLASHEx_Erase(&erase_init, &sector_error) != HAL_OK) {
-        HAL_FLASH_Lock();
-        return ErrorCode::FAILED;
-      }
-    }
+    HAL_FLASHEx_Erase(&flash_erase, &sector_error);
 
     HAL_FLASH_Lock();
+
     return ErrorCode::OK;
   }
 
-  ErrorCode Write(size_t offset, LibXR::ConstRawData data) override {
-    if ((offset + data.size_) > 2 * min_erase_size_) {
-      return ErrorCode::OUT_OF_RANGE;
+  ErrorCode Write(size_t offset, ConstRawData data) override {
+    uint8_t *data_u8 =
+        const_cast<uint8_t *>(reinterpret_cast<const uint8_t *>(data.addr_));
+
+    if (HAL_FLASH_Unlock() != HAL_OK) {
+      return ErrorCode::BUSY;
     }
-
-    offset += start_offset_;
-
-    HAL_FLASH_Unlock();
-
     __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
 
-    uint32_t addr = FLASH_BASE + offset;
-    const uint8_t *src = static_cast<const uint8_t *>(data.addr_);
+    uint32_t startaddr = offset + start_offset_ + FLASH_BASE;
+    uint32_t endaddr = startaddr + data.size_;
 
-    // G4 requires 64-bit aligned writes (double word)
-    while (addr < (FLASH_BASE + offset + data.size_)) {
-      uint64_t word = 0xFFFFFFFFFFFFFFFF;
-      memcpy(&word, src,
-             std::min<size_t>(8, (FLASH_BASE + offset + data.size_) - addr));
-
-      if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, addr, word) !=
+    while ((startaddr < endaddr)) {
+      if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, startaddr,
+                            *(reinterpret_cast<uint64_t *>(data_u8))) ==
           HAL_OK) {
-        HAL_FLASH_Lock();
-        return ErrorCode::FAILED;
+        data_u8 = data_u8 + 8;
+        startaddr = startaddr + 8;
+      } else {
+        break;
       }
-
-      addr += 8;
-      src += 8;
     }
 
     HAL_FLASH_Lock();
+
     return ErrorCode::OK;
   }
 
  private:
   size_t start_offset_;
-  size_t min_erase_size_;
 };
 
 __attribute__((unused)) static int STM32G4PowerFun(
